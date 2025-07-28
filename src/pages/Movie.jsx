dce19b1bb02cc7284ee64a9fd1d39a142a657c57
derrick <derrick.mwangi2@student.moringaschool.com>
2025-07-29 01:27:18 +0300

    Set up routing with Home, Directors, Actors, Movie, and ErrorPage

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from "react";
-
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+   useEffect(() => {
+    fetch(`http://localhost:3000/movies/${id}`)
+      .then(res => res.json())
+      .then(setMovie);
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
   return (
-    <>
+     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {movie.genres.map((genre, i) => (
+          <span key={i}>{genre} </span>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
